9342ba789e968a0f11f71e3c74da3743b1376d78
Siva Krishna <shivataraya@gmail.com>
2019-12-16 07:49:34 -0600

    add event handling

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,15 @@ import { User } from "./models/User";
 
 const user = new User({ name: "Shiva", age: 30 });
 
-user.set({ name: "shambho" });
-console.log(user.get("name"));
-console.log(user.get("age"));
-console.log("aum namah shivaya");
+user.on("shambho", () => {
+  console.log("shiva shambho");
+});
+user.on("shambho", () => {
+  console.log("aum namah shivaya");
+});
+user.on("shambho", () => {
+  console.log("mahadevaya");
+});
+
+user.trigger("shambho");
+console.log(user);
