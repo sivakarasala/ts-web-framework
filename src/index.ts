2cf04b4506f51520404818073e38db9fbcb3e49e
Siva Krishna <shivataraya@gmail.com>
2019-12-20 07:14:43 -0600

    extract attributes to generic class

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
 import { User } from "./models/User";
 
 const user = new User({ name: "siva", age: 0 });
-
-user.events.on("change", () => {
-  console.log("change!");
-});
-
-user.events.trigger("change");
