47705fb838f025570ad45e91cc4dbda36079ba14
Siva Krishna <shivataraya@gmail.com>
2019-12-22 07:20:32 -0600

    add data from user model on to dom

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
+import { User } from "./models/User";
 import { UserForm } from "./views/UserForm";
 
-const userForm = new UserForm(document.getElementById("root"));
+const user = User.buildUser({ name: "Marthanda", age: 25 });
+
+const userForm = new UserForm(document.getElementById("root"), user);
 
 userForm.render();
