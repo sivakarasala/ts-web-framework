d154105358e5c94e1ad7bd9dcc4ffe523960ccd2
Siva Krishna <shivataraya@gmail.com>
2019-12-22 07:39:44 -0600

    re-render on state update

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,22 +1,26 @@
 import { User } from "../models/User";
 
 export class UserForm {
-  constructor(public parent: Element, public model: User) {}
+  constructor(public parent: Element, public model: User) {
+    this.bindModel();
+  }
+
+  bindModel(): void {
+    this.model.on("change", () => {
+      this.render();
+    });
+  }
 
   eventsMap(): { [key: string]: () => void } {
     return {
-      "click:button": this.onButtonClick,
-      "mouseenter:h1": this.onMouseEnter
+      "click:.set-age": this.onSetAgeClick
     };
   }
 
-  onMouseEnter(): void {
-    console.log("Hara Hara");
-  }
+  onSetAgeClick = (): void => {
+    this.model.setRandomAge();
+  };
 
-  onButtonClick(): void {
-    console.log("Aum Namah Shivaya");
-  }
   template(): string {
     return `
             <div>
@@ -25,6 +29,7 @@ export class UserForm {
                 <div>User age: ${this.model.get("age")}</div>
                 <input />
                 <button>Click Me</button>
+                <button class="set-age">Set Random Age</button>
             </div>
         `;
   }
@@ -42,6 +47,7 @@ export class UserForm {
   }
 
   render(): void {
+    this.parent.innerHTML = "";
     const templateElement = document.createElement("template");
     templateElement.innerHTML = this.template();
 
