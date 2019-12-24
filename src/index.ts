5dbaa3e183d42a2d6dd09b8b03e50af9de8680fd
Siva Krishna <shivataraya@gmail.com>
2019-12-24 07:23:13 -0600

    add collectionview class

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
-import { User } from "./models/User";
-import { UserEdit } from "./views/UserEdit";
+import { UserList } from "./views/UserList";
+import { Collection } from "./models/Collection";
+import { UserProps, User } from "./models/User";
 
-const user = User.buildUser({ name: "Marthanda", age: 25 });
+const users = new Collection(
+  "http://localhost:3000/users",
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
 
-const root = document.getElementById("root");
+users.on("change", () => {
+  const root = document.getElementById("root");
 
-if (root) {
-  const userEdit = new UserEdit(root, user);
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
 
-  userEdit.render();
-} else {
-  throw new Error("Root element not found");
-}
+users.fetch();
